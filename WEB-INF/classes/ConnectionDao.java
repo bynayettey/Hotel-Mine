8e0d2d9606b9d12154abb536277c9ebf88c34a75
Bernard Ayettey <bernardnynayettey@gmail.com>
2024-02-29 13:52:37 +0000

    Hotelcommiit

---
+++ WEB-INF/classes/ConnectionDao.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+public class ConnectionDao implements ServletContextListener
+{
+	public void contextInitialized(ServletContextEvent e)
+	{
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection c=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hotel","hotel");
+			c.setHoldability(ResultSet.CLOSE_CURSORS_AT_COMMIT);
+			ServletContext ctx=e.getServletContext();
+			ctx.setAttribute("connection",c);
+		}	
+		catch(Exception ee){ee.printStackTrace();}
+	}
+	public void contextDestroyed(ServletContextEvent e)
+	{}
+}
